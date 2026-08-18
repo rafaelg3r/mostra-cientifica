982e902bca9e5f9a2380a246440f03634acfe412
Rafael C. Vedana <rafael.dacostavedana@gmail.com>
2026-08-18 01:23:28 -0300

    test

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/mostra-cientifica",
   reactCompiler: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
